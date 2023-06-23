7728c9c27d98ded7b5a960ff40998dc59e60a860
Nur Halisah Fasya <69709745+fasya22@users.noreply.github.com>
2023-06-23 14:53:30 +0700

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ class Registration(Resource):
 
             email_token = jwt.encode({"id": user_id}, jwt_secret_key, algorithm="HS256")
 
-            url = f"https://recycleai-api-production.up.railway.app/user/verify-account/{email_token}"
+            url = f"api-new-production.up.railway.app/user/verify-account/{email_token}"
 
             data = {
                 'name': name,
@@ -262,7 +262,7 @@ class ForgetPassword(Resource):
 
             email_token = jwt.encode({"id": user[0].id}, jwt_secret_key, algorithm="HS256")
 
-            url = f"https://recycleai-api-production.up.railway.app/user/reset-password/{email_token}"
+            url = f"api-new-production.up.railway.app/user/reset-password/{email_token}"
 
             sender = "noreply@app.com"
             msg = Message(subject="Reset your password", sender=sender, recipients=[email])
